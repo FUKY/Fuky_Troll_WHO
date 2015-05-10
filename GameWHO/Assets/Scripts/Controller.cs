3e651aef68f1d3d14b47a603e1581ad3c1128595
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2015-05-10 11:44:28 +0700

    Tao GamePlay - RanDom cac Type - Kiem tra khi nhan Button - Kiem tra GameOver

---
+++ GameWHO/Assets/Scripts/Controller.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+
+    public GameObject[] listTypeParent;
+    public GameObject[] listGear;
+    public RectTransform rectTransform;
+    public Transform transform;
+
+    public float timeDelayRandom;
+    private float timeDelay = 0;
+
+    public int indexButton = 0;
+    public int score;
+    public Text textScore;
+    void Awake()
+    {
+        score = 10;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        RandomType();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void RandomType()
+    {
+        int index;
+        int indexParent;
+        int indexGear;
+        index = Random.Range(0, 2);
+        if (index == 0)
+        {
+            indexParent = Random.Range(0, 3);
+            GameObject type = Instantiate(listTypeParent[indexParent], transform.position, Quaternion.identity) as GameObject;
+            type.transform.SetParent(rectTransform);
+            //type.transform.localPosition = Vector3.zero;
+            type.transform.localScale = Vector3.one;
+            type.GetComponent<TypeController>().inDex = index;
+        }
+        if(index == 1)
+        {
+            indexGear = Random.Range(0, 3);
+            GameObject type = Instantiate(listGear[indexGear], transform.position, Quaternion.identity) as GameObject;
+            type.transform.SetParent(rectTransform);
+            //type.transform.localPosition = Vector3.zero;
+            type.transform.localScale = Vector3.one;
+            type.GetComponent<TypeController>().inDex = index;
+        }
+        
+    }
+    public void GameOver()
+    {
+        Debug.Log("GAME OVER");
+    }
+    public void AddScore()
+    {
+        
+    }
+    public void CheckButtonParent()
+    {
+        indexButton = 0;
+        TypeController type = gameObject.GetComponentInChildren<TypeController>();
+        if (type == null)
+        {
+            Debug.Log("hehe");
+        }
+        Debug.Log("haha");
+        type.CheckIndex(indexButton);
+
+    }
+    public void CheckButtonGear()
+    {
+        indexButton = 1;
+        TypeController type = gameObject.GetComponentInChildren<TypeController>();
+        if (type == null)
+        {
+            Debug.Log("hehe");
+        }
+        Debug.Log("haha");
+        type.CheckIndex(indexButton);
+    }
+    void OnTriggerEnter2D()
+    {
+        GameOver();
+    }
+}
---
+++ GameWHO/Assets/Scripts/TypeController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TypeController : MonoBehaviour {
+
+    public int inDex;
+    private Controller control;
+	// Use this for initialization
+	void Start () {
+        control = gameObject.GetComponentInParent<Controller>();
+        if (control == null)
+        {
+            Debug.Log("chua co");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void CheckIndex(int indexButton)
+    {
+        if (inDex == indexButton)
+        {
+            control.AddScore();
+            control.RandomType();
+            Destroy(gameObject);
+        }
+        else
+        {
+            control.GameOver();
+        }
+    }
+}
